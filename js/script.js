b271ab6ec30ce0a4dfec5018284924f29c0450d1
dwab <dan.cicconi@gooduncle.com>
2019-03-26 16:36:59 -0400

    Remove stellar() jQuery plugin

--- js/script.js
+++ js/script.js
@@ -69,33 +69,6 @@ jQuery(document).ready(function ($) {
     $("#button").addClass('button');
   }, 5000);
 
-  var isMobile = {
-    Android: function() {
-      return navigator.userAgent.match(/Android/i);
-    },
-    BlackBerry: function() {
-      return navigator.userAgent.match(/BlackBerry/i);
-    },
-    iOS: function() {
-      return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-    },
-    Opera: function() {
-      return navigator.userAgent.match(/Opera Mini/i);
-    },
-    Windows: function() {
-      return navigator.userAgent.match(/IEMobile/i);
-    },
-    any: function() {
-      return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
-    }
-  };
-
-  if( !isMobile.any() ){
-    $(window).stellar();
-  }
-
-  $(window).stellar();
-
   var waypointLinks = $('.js-waypoint__link'),
       slide = $('.slide'),
       button = $('.button'),
